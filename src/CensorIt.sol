df8f025978304bfcae70bb0126ccd47b6b19ed6e
Saksham Bhugra <sakshambhugra8@gmail.com>
2024-10-28 21:25:30 +0000

    Chore: added flattened version of contracts

--- src/CensorIt.sol
+++ src/CensorIt.sol
@@ -3,10 +3,17 @@
 pragma solidity ^0.8.20;
 
 import {ECDSAServiceManagerBase} from "../lib/eigenlayer-middleware/src/unaudited/ECDSAServiceManagerBase.sol";
+import {ECDSAStakeRegistry} from "../lib/eigenlayer-middleware/src/unaudited/ECDSAStakeRegistry.sol";
+import {ECDSAUpgradeable} from "@openzeppelin-upgrades/contracts/utils/cryptography/ECDSAUpgradeable.sol";
+import {IERC1271Upgradeable} from "@openzeppelin-upgrades/contracts/interfaces/IERC1271Upgradeable.sol";
+import {OperatorAllowlist } from "../lib/wizard-templates/src/templates/OperatorAllowlist.sol";
 
-contract Censorlt{
+contract Censorlt is ECDSAServiceManagerBase,OperatorAllowlist {
+
+    using ECDSAUpgradeable for bytes32;
+    // ECDSAStakeRegistry public stakeRegistry;
     enum VoilationType{
-        SPAM,
+        VULGAR,
         FAKE,
         HARMFUL,
         COPYRIGHT
@@ -17,103 +24,128 @@ contract Censorlt{
         TEXT,
         IMAGE
     }
-    struct Operator{
-        uint256 stake;   
-        uint256 reputation; 
-        uint256 totalVotes;   
-        uint256 correctVotes;
-    }
+    
     struct ContentReport{
         string content;
         ContentType contentType;
         address reporter;
         VoilationType voilationType;
         bool isResolved;
-        uint rewardAmount;
-        uint removeCount;
-        uint keepCount;
+        uint256 removeCount;
+        uint256 keepCount;
         address[] hasVoted;
-        mapping(address=>bool) removeVote;
+        uint32 taskCreatedBlock;
     }
-    uint numReports;
-    mapping(uint=>ContentReport) public reports;
-    mapping(address => Operator) public registeredOperators;
-    uint public minStakeRequired= 10000000000000000;
-    address public owner;
-    constructor(){
-        owner= msg.sender;
+
+    event newReportCreated(uint256 indexed _numReports,ContentReport indexed _newReport);
+    event TaskResponded(uint256 indexed _reportIndex,ContentReport indexed _report, address indexed _operator);
+
+    mapping(uint256 => bytes32) public allTaskHashes;
+    mapping(address => mapping(uint256 => bytes)) public allTaskResponses;
+
+    modifier onlyOperator() {
+        require(
+            ECDSAStakeRegistry(stakeRegistry).operatorRegistered(msg.sender),
+            "Operator must be the caller"
+        );
+        _;
     }
-    function reportContent(string memory _content, VoilationType _voilationType, ContentType _contentType) external payable{
-        require(msg.value>0,"stake required");
-        ContentReport storage newReport= reports[numReports++];
+
+    uint256 public numReports;
+    mapping(uint256=>ContentReport) public reports;
+
+    constructor(
+        address _avsDirectory,
+        address _stakeRegistry,
+        address _rewardsCoordinator,
+        address _delegationManager
+    )ECDSAServiceManagerBase(
+            _avsDirectory,
+            _stakeRegistry,
+            _rewardsCoordinator,
+            _delegationManager
+    ){}
+
+     function initialize(address initialOwner_, address rewardsInitiator_, address allowlistManager_)
+        external
+        initializer
+    {
+        __ServiceManagerBase_init(initialOwner_, rewardsInitiator_);
+        __OperatorAllowlist_init(allowlistManager_, true);
+    }
+
+    function createNewTask(string memory _content, VoilationType _voilationType, ContentType _contentType) external {
+        // create a new task struct
+        ContentReport memory newReport;
         newReport.content=_content;
         newReport.reporter=msg.sender;
         newReport.contentType=_contentType;
         newReport.voilationType=_voilationType;
-        newReport.rewardAmount=msg.value;
+        newReport.taskCreatedBlock = uint32(block.number);
+        
+        // store hash of task onchain, emit event, and increase taskNum
+        allTaskHashes[numReports] = keccak256(abi.encode(newReport));
+        reports[numReports] = newReport;
+        emit newReportCreated(numReports, newReport);
+        numReports = numReports + 1;
     }
-    function alreadyVoted(uint _contentId,address _operator) internal view returns(bool){
-        ContentReport storage report= reports[_contentId];
-        for(uint i=0;i<report.hasVoted.length;i++){
+
+    function alreadyVoted(uint256 _contentId,address _operator) internal view returns(bool){
+        ContentReport memory report= reports[_contentId];
+        for(uint256 i=0;i<report.hasVoted.length;i++){
             if(report.hasVoted[i]==_operator){
                 return true;
             }
         }
         return false;
     }
-    function voteAsOperator(uint _contentId,bool _removeCount) external{
-        require(isOperator(msg.sender),"Only Operater can vote");
-        ContentReport storage report= reports[_contentId];
-        require(! alreadyVoted(_contentId, msg.sender),"already voted");
-        if(_removeCount){
+    
+    function respondToTask(
+        uint256 _contentId,
+        bool _vote,
+        bytes memory signature
+    ) external onlyOperator {
+
+        // check that the task is valid, hasn't been responsed yet, and is being responded in time
+        require(!alreadyVoted(_contentId, msg.sender), "already voted");
+        require(
+            allTaskResponses[msg.sender][numReports].length == 0,
+            "Operator has already responded to the task"
+        );
+
+        // The message that was signed
+        bytes32 messageHash = keccak256(
+            abi.encodePacked(
+                _contentId,
+                _vote,
+                msg.sender
+            )
+        );
+
+        bytes32 ethSignedMessageHash = messageHash.toEthSignedMessageHash();
+        
+        // Verify signature using the stake registry
+        bytes4 magicValue = IERC1271Upgradeable.isValidSignature.selector;
+        require(
+            magicValue == ECDSAStakeRegistry(stakeRegistry).isValidSignature(ethSignedMessageHash, signature),
+            "Invalid signature"
+        );
+        ContentReport storage report = reports[_contentId];
+        // updating the storage with task responses
+        allTaskResponses[msg.sender][numReports] = signature;
+        
+        if (!_vote) {
             report.removeCount++;
-            report.removeVote[msg.sender]=true;
-        }
-        else{
+        } else {
             report.keepCount++;
-            report.removeVote[msg.sender]=false;
         }
         report.hasVoted.push(msg.sender);
-        // uint totalVotes= report.removeCount+report.keepCount;
-        // if(totalVotes>=3){
-        //     bool remove= report.removeCount>report.keepCount;
-        //     report.isResolved=true;
-        //     // distributeRewards(_contentId, remove);
-        // }
-    }
-    
-    function setMinStake(uint256 _newMinStake) external {
-        require(msg.sender==owner);
-        minStakeRequired = _newMinStake;
+
+        // emitting event
+        emit TaskResponded(numReports, report, msg.sender);
     }
 
-    function registerAsOperator() external payable{
-         require(msg.value >= minStakeRequired, "Not enough stake");
-        Operator storage newOperator= registeredOperators[msg.sender];
-        newOperator.stake=msg.value;
+    function getReport(uint256 _contentId) public view returns(ContentReport memory){
+        return reports[_contentId];
     }
-
-     function isOperator(address _operator) internal view returns(bool){
-        if(registeredOperators[_operator].stake>=minStakeRequired){
-            return true;
-        }
-        return false;
-    }
-
-    // function distributeRewards(uint _contentId,bool _remove) internal{
-    //     ContentReport storage report= reports[_contentId];
-    //     uint256 correctVoterCount = 0;
-    //     for(uint i=0;i<report.hasVoted.length;i++){
-    //         if(report.removeVote[report.hasVoted[i]]==_remove){
-    //             correctVoterCount++;
-    //         }
-    //     }
-    //      uint256 rewardPerCorrectVoter = report.rewardAmount / correctVoterCount;
-    //      for(uint i=0;i<report.hasVoted.length;i++){
-    //         if(report.removeVote[report.hasVoted[i]]==_remove){
-    //             (bool success,)=payable(report.hasVoted[i]).call{value:rewardPerCorrectVoter}("");
-    //             require(success);
-    //         }
-    //     }
-    // }
 }
